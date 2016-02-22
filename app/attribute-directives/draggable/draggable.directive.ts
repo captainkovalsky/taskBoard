42d6bfce4ae5f8e449adb7566e1e896a3aaaca4f
Viktor Dzundza <victor.dzundza.dev@gmail.com>
2016-02-23 00:33:40 +0200

    Add draggab;le

--- app/attribute-directives/draggable/draggable.directive.ts
+++ app/attribute-directives/draggable/draggable.directive.ts
@@ -3,26 +3,49 @@ import {Directive, ElementRef, Input} from 'angular2/core';
     selector: '[myDraggable]',
     host: {
     '(mousedown)': 'onMouseDown($event)',
-    '(mousemove)': 'onMouseMove()',
-    '(mouseup)': 'onMouseUp()'
+    '(mousemove)': 'onMouseMove($event)',
+    '(mouseup)': 'endDrag($event)'
    }
 })
 export class DraggableDirective {
-    constructor(el: ElementRef) {
-      let startX = 0, startY = 0, x = 0, y = 0;
-      const elementNative = el.nativeElement;
-      elementNative.style.position = 'relative';
+    private startX: number;
+    private startY: number;
+    private x: number;
+    private y: number;
+    private isMoved: boolean;
+
+    private zIndex: Number;
+
+    constructor(private el: ElementRef) {
+      this.isMoved = false;
+      this.startX = 0;
+      this.startY = 0;
+      this.x = 0;
+      this.y = 0;
+      el.nativeElement.style.position = 'relative';
     }
 
     onMouseDown($event: MouseEvent) {
-      console.log('mouse down', $event);
+      event.preventDefault();
+      this.startX = $event.clientX - this.x;
+      this.startY = $event.clientY - this.y;
+      this.isMoved = true;
+      this.el.nativeElement.style.zIndex += 1;
     }
 
-    onMouseMove($event: MouseEvent) {
-      console.log('mouse down', $event);
-    }
+    onMouseMove($event: MouseEvent, el: ElementRef) {
+      if (!this.isMoved) {
+        return;
+      }
 
-    onMouseUp($event: MouseEvent) {
-      console.log('mouse down', $event);
+      this.x = $event.clientX - this.startX;
+      this.y = $event.clientY - this.startY;
+      this.el.nativeElement.style.top = `${this.y}px`;
+      this.el.nativeElement.style.left = `${this.x}px`;
+  }
+
+    endDrag($event: MouseEvent) {
+      this.isMoved = false;
+      this.el.nativeElement.style.zIndex -= 1;
     }
 }
